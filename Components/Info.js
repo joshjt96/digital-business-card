af5b25c30b319cd1f936244204de27abcc1904f9
Josh Talley <jtdev84@gmail.com>
2022-10-27 15:22:01 +0100

    Create Info component and CSS

--- Components/Info.js
+++ Components/Info.js
@@ -4,7 +4,11 @@ import ReactDOM from "react-dom"
 
 function Info() {
     return (
-    
+        <div className="info-div">
+            <h1>Your Name</h1>
+            <h2>Your Job Title</h2>
+            <a href="/">yourwebsite.dev</a>
+        </div>
     )
 }
 
