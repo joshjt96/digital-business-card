afd6b8fe08494ac0cece7c2f3165cd6a0d39c99f
Josh Talley <jtdev84@gmail.com>
2022-10-27 16:01:40 +0100

    Create About component and CSS

--- Components/About.js
+++ Components/About.js
@@ -4,7 +4,12 @@ import ReactDOM from "react-dom"
 
 function About() {
     return (
-    
+        <div className="about-div">
+            <h3>About</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus leo et condimentum accumsan. Praesent vel scelerisque diam, et interdum odio. Cras justo risus, finibus sed odio ac, facilisis vehicula metus.</p>
+            <h3>Interests</h3>
+            <p>Donec hendrerit justo dui, et varius metus ultricies ut. Vivamus rhoncus at nisi sed varius. Etiam ut lorem nulla. Curabitur at arcu vel erat malesuada porttitor quis in leo. </p>
+        </div>
     )
 }
 
